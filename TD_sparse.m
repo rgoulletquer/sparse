fe5e9c587629320fe1a816d7e25f3489d73872db
rjuge <remi_juge@hotmail.fr>
2018-01-23 09:54:51 +0100

    Add processing on blocks

--- TD_sparse.m
+++ TD_sparse.m
@@ -8,11 +8,12 @@ colorbar
 
 %% FFT
 I_FFT = fft2(I);
-I_FFT = fftshift(I_FFT);
+I_FFTSHIFT = fftshift(I_FFT);
+
 I_abs = log(abs(I_FFT));
 %I_abs = mat2gray(I_abs);
 figure
-imshow(mat2gray(I_abs), [])
+imshow(mat2gray(log(abs(I_FFTSHIFT))), [])
 colorbar
 title('abs(FFT)')
 % Il y a deux directions dominantes dans la FFT du fait de la forme
@@ -46,7 +47,7 @@ colorbar
 title('reconstruction from magnitude and phase')
 
 %% DCT
-I_DCT = ((dct2(I)));
+I_DCT = dct2(I);
 
 figure
 imshow(log(abs(I_DCT)), [])
@@ -76,23 +77,79 @@ subplot(3,2,5), imagesc(I_Low), title('LF'), colorbar
 subplot(3,2,6), imagesc(I_High), title('HF'), colorbar
 
 
-% seuil
-s=[0 1 2 5 10 20 50 100 200];
+%% threshold
+s_FFT = linspace(1, max(I_FFT(:))/1000, 9);
+s_DCT = linspace(1, max(I_DCT(:))/1000, 9);
+
+% initialisation
 j = 1;
-cnt = 0;
-t1 = zeros(size(s));
-SNR = zeros(size(s));
+t1_FFT = zeros(1, 9);
+e_FFT = zeros(1, 9);
 figure
-for i = s
-I_DCT(I_DCT < i) = 0.0;
-Irec_DCT = double(uint8(idct2(I_DCT)));
-cnt = sum(Irec_DCT(:) == 0);
-t1(j) = 1 - cnt/(w*h);
-SNR(j) = snr(I, Irec_DCT);
+for i = s_FFT
+i_fft = I_FFT;
+% threshold
+i_fft(abs(i_fft) < i) = 0.0;
+% count nb of coef set to 0
+cnt_FFT = sum(i_fft(:) == 0);
+% reconstruction
+Irec_FFT = double((ifft2(i_fft)));
+% compression ratio and error
+t1_FFT(j) =  (w*h)/cnt_FFT;
+e_FFT(j) = norm(I - Irec_FFT, 'fro');
+% plot
 subplot(3, 3, j)
-imshow(Irec_DCT, [])
+imshow(Irec_FFT, [])
+colorbar
+title(['FFT reconstruction with seuil = ', num2str(i)])
+j = j+1;
+end
+
+% initialisation
+j = 1;
+t1_DCT = zeros(1,9);
+e_DCT = zeros(1,9);
+figure
+for i = s_DCT
+i_dct = I_DCT;
+% threshold
+i_dct(abs(i_dct) < i) = 0.0;
+% count nb of coef set to 0
+cnt_DCT = sum(i_dct(:) == 0);
+% reconstruction
+Irec_DCT = double((idct2(i_dct)));
+% compression ratio and error
+t1_DCT(j) = (w*h)/cnt_DCT;
+e_DCT(j) = norm(I - Irec_DCT, 'fro');
+% plot
+subplot(3, 3, j)
+imshow(uint8(Irec_DCT), [])
 colorbar
 title(['DCT reconstruction with seuil = ', num2str(i)])
 j = j+1;
 end
 
+figure
+plot(t1_FFT, e_FFT, t1_DCT, e_DCT)
+
+%% FFT block processing
+
+fft2_block = @(block_struct) fft2(block_struct.data);
+ifft2_block = @(block_struct) ifft2(block_struct.data);
+I_FFTBLOCK = blockproc(I, [9 9], fft2_block);
+Irec_fftblock = blockproc(I_FFTBLOCK, [9 9], ifft2_block);
+figure
+imshow(uint8(Irec_fftblock), [])
+colorbar
+title('reconstruction from fft on blocks')
+
+%% DCT block processing
+
+dct2_block = @(block_struct) dct2(block_struct.data);
+idct2_block = @(block_struct) idct2(block_struct.data);
+I_DCTBLOCK = blockproc(I, [9 9], dct2_block);
+Irec_dctblock = blockproc(I_DCTBLOCK, [9 9], idct2_block);
+figure
+imshow(uint8(Irec_dctblock), [])
+colorbar
+title('reconstruction from dct on blocks')
